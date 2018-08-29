3bae8c13364536abd4acfe9b63ee43d0159a0230
michalbiesek <michal.biesek@intel.com>
2018-08-29 11:17:59 +0200

    tcache support for realloc

--- deps/memkind/src/memkind_arena.c
+++ deps/memkind/src/memkind_arena.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 - 2017 Intel Corporation.
+ * Copyright (C) 2014 - 2018 Intel Corporation.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -415,7 +415,7 @@ MEMKIND_EXPORT void *memkind_arena_realloc(struct memkind *kind, void *ptr, size
                 ptr = jemk_mallocx_check(size, MALLOCX_ARENA(arena) | get_tcache_flag(kind->partition, size));
             }
             else {
-                ptr = jemk_rallocx_check(ptr, size, MALLOCX_ARENA(arena));
+                ptr = jemk_rallocx_check(ptr, size, MALLOCX_ARENA(arena) | get_tcache_flag(kind->partition, size));
             }
         }
     }
