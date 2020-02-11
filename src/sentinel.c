25f80f930b1263b21047d54be7a6b6cc8d9a7083
Pawel Karpinski <pawel.karpinski@intel.com>
2020-02-11 10:23:07 -0500

    Move dictEntry to PM
    dictEntry instances will be allocated to PM. Involves only main dictionary entries.

--- src/sentinel.c
+++ src/sentinel.c
@@ -3853,7 +3853,7 @@ int sentinelLeaderIncr(dict *counters, char *runid) {
     dictEntry *existing, *de;
     uint64_t oldval;
 
-    de = dictAddRaw(counters,runid,&existing);
+    de = dictAddRaw(counters,runid,&existing,DICT_ENTRIES_ON_DRAM);
     if (existing) {
         oldval = dictGetUnsignedIntegerVal(existing);
         dictSetUnsignedIntegerVal(existing,oldval+1);
