c2f461c3d95f3e54b53759b56c3e473589758ccc
michalbiesek <michal.biesek@intel.com>
2020-02-17 15:33:37 +0100

    Fix setting pmem threshold in zmalloc module

--- src/server.c
+++ src/server.c
@@ -2941,7 +2941,7 @@ void initServer(void) {
     scriptingInit(1);
     slowlogInit();
     latencyMonitorInit();
-    zmalloc_set_threshold(server.pmem_threshold);
+    pmemThresholdInit();
 }
 
 /* Some steps in server initialization need to be done last (after modules
