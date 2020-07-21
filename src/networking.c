d7a86425d5b0a7610d71f3877dbba32bb2ca9323
jschmieg <jakub.schmiegel@intel.com>
2020-07-21 05:37:38 -0400

    Replace free_dram with free to fix memory usage calculation

--- src/networking.c
+++ src/networking.c
@@ -1006,7 +1006,7 @@ void acceptUnixHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
 static void freeClientArgv(client *c) {
     int j;
     for (j = 0; j < c->argc; j++)
-        decrRefCountDRAM(c->argv[j]);
+        decrRefCount(c->argv[j]);
     c->argc = 0;
     c->cmd = NULL;
 }
@@ -1206,7 +1206,7 @@ void freeClient(client *c) {
     /* Release other dynamically allocated client structure fields,
      * and finally release the client structure itself. */
     if (c->name) decrRefCount(c->name);
-    zfree_dram(c->argv);
+    zfree(c->argv);
     freeClientMultiState(c);
     sdsfree(c->peerid);
     zfree_dram(c);
@@ -1541,7 +1541,7 @@ int processInlineBuffer(client *c) {
 
     /* Setup argv array on client structure */
     if (argc) {
-        if (c->argv) zfree_dram(c->argv);
+        if (c->argv) zfree(c->argv);
         c->argv = zmalloc_dram(sizeof(robj*)*argc);
     }
 
@@ -1638,7 +1638,7 @@ int processMultibulkBuffer(client *c) {
         c->multibulklen = ll;
 
         /* Setup argv array on client structure */
-        if (c->argv) zfree_dram(c->argv);
+        if (c->argv) zfree(c->argv);
         c->argv = zmalloc_dram(sizeof(robj*)*c->multibulklen);
     }
 
@@ -2587,7 +2587,7 @@ void rewriteClientCommandVector(client *c, int argc, ...) {
      * sure that if the same objects are reused in the new vector the
      * refcount gets incremented before it gets decremented. */
     for (j = 0; j < c->argc; j++) decrRefCount(c->argv[j]);
-    zfree_dram(c->argv);
+    zfree(c->argv);
     /* Replace argv and argc with our new versions. */
     c->argv = argv;
     c->argc = argc;
@@ -2599,7 +2599,7 @@ void rewriteClientCommandVector(client *c, int argc, ...) {
 /* Completely replace the client command vector with the provided one. */
 void replaceClientCommandVector(client *c, int argc, robj **argv) {
     freeClientArgv(c);
-    zfree_dram(c->argv);
+    zfree(c->argv);
     c->argv = argv;
     c->argc = argc;
     c->cmd = lookupCommandOrOriginal(c->argv[0]->ptr);
@@ -2621,7 +2621,7 @@ void rewriteClientCommandArgument(client *c, int i, robj *newval) {
     robj *oldval;
 
     if (i >= c->argc) {
-        c->argv = zrealloc_dram(c->argv,sizeof(robj*)*(i+1));
+        c->argv = zrealloc(c->argv,sizeof(robj*)*(i+1));
         c->argc = i+1;
         c->argv[i] = NULL;
     }
